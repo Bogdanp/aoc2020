8a43b3e6fc7c7bad6c4f050f4b05c542e41b9977
Bogdan Popa <bogdan@defn.io>
2020-12-01 22:00:55 +0200

    add a generalized solution to day 1

--- day01.rkt
+++ day01.rkt
@@ -1,7 +1,7 @@
-#lang racket/base
+#lang racket
 
 (define nums
-  (call-with-input-file "input1.txt"
+  (call-with-input-file "input01.txt"
     (lambda (in)
       (for/list ([line (in-lines in)])
         (string->number line)))))
@@ -19,3 +19,25 @@
              #:unless (eqv? b c)
              #:when (= 2020 (+ a b c)))
   (* a b c))
+
+
+;; Generalized ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-syntax (find-solution stx)
+  (syntax-case stx ()
+    [(_ nums ns)
+     (let ([make-ids (lambda (p)
+                       (for/list ([i (in-range (syntax->datum #'ns))])
+                         (string->symbol (format "~a~a" p i))))])
+       (with-syntax ([(n-id ...) (make-ids "n-")]
+                     [(p-id ...) (make-ids "p-")])
+         #'(let ([xs nums])
+             (for*/first ([(n-id p-id) (in-indexed xs)] ...
+                          #:when (= ns (length (remove-duplicates (list p-id ...))))
+                          #:when (= 2020 (+ n-id ...)))
+               (* n-id ...)))))]))
+
+(find-solution nums 1)
+(find-solution nums 2)
+(find-solution nums 3)
+(find-solution nums 4)
