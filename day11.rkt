e3ff6e1718b7d08fc585b33aa50477f4cb32c3e6
Bogdan Popa <bogdan@defn.io>
2020-12-11 23:44:10 +0200

    clean up day 11 sln

--- day11.rkt
+++ day11.rkt
@@ -1,8 +1,5 @@
 #lang racket/base
 
-(require racket/match
-         racket/port)
-
 (define matrix
   (call-with-input-file "input11.txt"
     (lambda (in)
@@ -13,30 +10,33 @@
 (define w (vector-length (vector-ref matrix 0)))
 (define h (vector-length matrix))
 
-(define (pos x y)
-  (vector-ref (vector-ref matrix y) x))
+(define (ref m x y)
+  (vector-ref (vector-ref m y) x))
 
-(define (floor?    x y) (char=? (pos x y) #\.))
-(define (empty?    x y) (char=? (pos x y) #\L))
-(define (occupied? x y) (char=? (pos x y) #\#))
+(define (floor?    c) (char=? c #\.))
+(define (empty?    c) (char=? c #\L))
+(define (occupied? c) (char=? c #\#))
 
-(define (count-adjacent x y)
+(define (count-adjacent m x y)
   (for*/sum ([x* (in-range (max 0 (sub1 x)) (add1 (min (sub1 w) (add1 x))))]
              [y* (in-range (max 0 (sub1 y)) (add1 (min (sub1 h) (add1 y))))]
              #:unless (and (= x x*)
                            (= y y*)))
-    (if (occupied? x* y*) 1 0)))
+    (if (occupied? (ref m x* y*)) 1 0)))
 
-(define (count-adjacent-by-sight x y)
+(define (count-adjacent-by-sight m x y)
   (define (help x y step-x step-y)
     (define new-x (+ x step-x))
     (define new-y (+ y step-y))
     (cond
       [(or (< new-x 0) (= new-x w)) 0]
       [(or (< new-y 0) (= new-y h)) 0]
-      [(empty?    new-x new-y) 0]
-      [(occupied? new-x new-y) 1]
-      [else (help new-x new-y step-x step-y)]))
+      [else
+       (define c (ref m new-x new-y))
+       (cond
+         [(empty?    c) 0]
+         [(occupied? c) 1]
+         [else (help new-x new-y step-x step-y)])]))
   (+ (help x y  1  0)
      (help x y -1  0)
      (help x y  0  1)
@@ -46,55 +46,30 @@
      (help x y  1 -1)
      (help x y -1  1)))
 
-(define (leave!  x y) (vector-set! (vector-ref matrix y) x #\L))
-(define (occupy! x y) (vector-set! (vector-ref matrix y) x #\#))
-
-(define (step [ca count-adjacent] [tolerance 4])
-  (define instructions
-    (for*/list ([x (in-range w)]
-                [y (in-range h)]
-                #:unless (floor? x y))
+(define (step m [ca count-adjacent] [t 4])
+  (for/vector #:length h ([y (in-range h)])
+    (for/vector #:length w ([x (in-range w)])
+      (define c (ref m x y))
       (cond
-        [(empty? x y)
-         (if (zero? (ca x y))
-             (list 'occupy x y)
-             (list 'noop   x y))]
-        [(occupied? x y)
-         (if (>= (ca x y) tolerance)
-             (list 'leave x y)
-             (list 'noop  x y))]
-        [else
-         (list 'noop x y)])))
-  (for ([i (in-list instructions)])
-    (match i
-      [(list 'occupy x y) (occupy! x y)]
-      [(list 'leave  x y) (leave!  x y)]
-      [_ (void)]))
-  (matrix->string))
+        [(floor?    c) #\.]
+        [(empty?    c) (if (zero? (ca m x y)) #\# c)]
+        [(occupied? c) (if (>= (ca m x y) t)  #\L c)]
+        [else c]))))
 
-(define (matrix->string)
-  (call-with-output-string
-   (lambda (out)
-     (for ([(row i) (in-indexed matrix)])
-       (when (> i 0)
-         (newline out))
-       (for ([col (in-vector row)])
-         (display col out))))))
-
-(define (occupied-count)
+(define (count-occupied m)
   (for*/sum ([x (in-range w)]
              [y (in-range h)])
-    (if (occupied? x y) 1 0)))
+    (if (occupied? (ref m x y)) 1 0)))
 
-(define (step-until-stable [ca count-adjacent] [tolerance 4])
-  (let loop ([seen (list (matrix->string))])
+(define (step-until-stable m [ca count-adjacent] [tolerance 4])
+  (let loop ([m m])
     (define new-matrix
-      (step ca tolerance))
+      (step m ca tolerance))
     (cond
-      [(member new-matrix seen)
-       (occupied-count)]
+      [(equal? m new-matrix)
+       (count-occupied m)]
       [else
-       (loop (cons new-matrix seen))])))
+       (loop new-matrix)])))
 
-#;(step-until-stable)
-#;(step-until-stable count-adjacent-by-sight 5)
+(time (step-until-stable matrix))
+(time (step-until-stable matrix count-adjacent-by-sight 5))
